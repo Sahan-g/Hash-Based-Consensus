75ad6dd95f62c1dd4dc3b17889f8b4885e5f8881
kalangi <thathsarakalangi@gmail.com>
2025-07-29 19:47:40 +0530

    Implemented the transaction pool and wallet

--- app/index.js
+++ app/index.js
@@ -1,45 +1,79 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const Block = require('../blockchain/block');
-const Blockchain = require('../blockchain/index');
+const express = require("express");
+const bodyParser = require("body-parser");
+const Block = require("../blockchain/block");
+const Blockchain = require("../blockchain/index");
+
+const Wallet = require("../wallet");
+const TransactionPool = require("../transaction/transaction-pool");
+const { DIFFICULTY } = require("../config");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 const startServer = () => {
-    app.use(bodyParser.json());
+  app.use(bodyParser.json());
 
-    const blockchain = new Blockchain();
+  const blockchain = new Blockchain();
+  const wallet = new Wallet();
+  const tp = new TransactionPool();
 
-    // app.post('/createBlock', (req, res) => {
-    //     const { index, timestamp, transactions, previousHash, proposerPublicKey } = req.body;
-    //     try {
-    //         console.log('Creating a new block');
-    //         const newBlock = new Block({ index, timestamp, transactions, previousHash, proposerPublicKey });
-    //         res.status(201).json(newBlock);
-    //     } catch (error) {
-    //         res.status(400).json({ error: 'Invalid block data' });
-    //     }
-    // });
+  // app.post('/createBlock', (req, res) => {
+  //     const { index, timestamp, transactions, previousHash, proposerPublicKey } = req.body;
+  //     try {
+  //         console.log('Creating a new block');
+  //         const newBlock = new Block({ index, timestamp, transactions, previousHash, proposerPublicKey });
+  //         res.status(201).json(newBlock);
+  //     } catch (error) {
+  //         res.status(400).json({ error: 'Invalid block data' });
+  //     }
+  // });
 
-    // app.post('/addBlock', (req, res) => {
-    //     const block = req.body;
-    //     try {
-    //         blockchain.addBlock(new Block(block));
-    //         res.status(200).json({ message: 'Block added successfully', block });
-    //     } catch (error) {
-    //         res.status(400).json({ error: error.message });
-    //     }
-    // })
+  // app.post('/addBlock', (req, res) => {
+  //     const block = req.body;
+  //     try {
+  //         blockchain.addBlock(new Block(block));
+  //         res.status(200).json({ message: 'Block added successfully', block });
+  //     } catch (error) {
+  //         res.status(400).json({ error: error.message });
+  //     }
+  // })
 
-    // app.get('/chain', (_req, res) => {
-    //     res.status(200).json(blockchain.chain);
-    // })
-    
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-}
+  // app.get('/chain', (_req, res) => {
+  //     res.status(200).json(blockchain.chain);
+  // })
+
+  // app.get("/transactions", (req, res) => {
+  //   res.json(tp.transactions);
+  // });
+
+  // app.post("/transact", (req, res) => {
+  //   const { recipient, amount } = req.body;
+
+  //   if (!recipient || !amount) {
+  //     return res.status(400).send("Recipient and amount are required.");
+  //   }
+
+  //   const transaction = wallet.createTransaction(
+  //     recipient,
+  //     amount,
+  //     tp,
+  //     blockchain
+  //   );
+  //   if (transaction) {
+  //     console.log("Transaction created:", transaction);
+  //   }
+
+  //   res.redirect("/transactions");
+  // });
+
+  // app.get("/public-key", (req, res) => {
+  //   res.json({ publicKey: wallet.publicKey });
+  // });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
 
 startServer();
