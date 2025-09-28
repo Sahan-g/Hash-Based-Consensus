8f5fea59b214f4c5f0c2bce0ded2a268648a35bb
kalangi <thathsarakalangi@gmail.com>
2025-09-28 13:29:24 +0530

    feat : consensus flow

--- proof-of-luck/luck-node.js
+++ proof-of-luck/luck-node.js
@@ -30,6 +30,57 @@ class LuckNode {
     return proposal;
   }
 
+  createProposalWithLuck(round) {
+    const lastBlock = this.blockchain.getLastBlock();
+
+    const transactions = [
+      {
+        id: "tx1",
+        timestamp: 1695812331234,
+        sensor_id: "sensor-A1",
+        reading: { temperature: 26.4, humidity: 58 },
+        metadata: { location: "Colombo", unit: "Celsius" },
+        hash: "a2f31bc...",
+        input: {
+          timestamp: 1695812331234,
+          address: "04ab23f...publicKey",
+          signature: "3045022100ff...",
+        },
+      },
+      {
+        id: "tx2",
+        timestamp: 1695812398888,
+        sensor_id: "sensor-B2",
+        reading: { soilMoisture: 33 },
+        metadata: null,
+        hash: "c73b92e...",
+        input: {
+          timestamp: 1695812398888,
+          address: "04cd98e...publicKey",
+          signature: "3045022100aa...",
+        },
+      },
+    ];
+
+    const blockCandidate = new Block({
+      index: lastBlock.index + 1,
+      transactions,
+      previousHash: lastBlock.hash,
+      proposerPublicKey: this.wallet.publicKey,
+      wallet: this.wallet,
+    });
+
+    const luckProof = Luck.generateLuck(this.wallet, round);
+    blockCandidate.luckProof = luckProof;
+
+    const proposal = {
+      block: blockCandidate,
+      round,
+    };
+
+    return proposal;
+  }
+
   verifyProposal(proposal) {
     const { block, luckProof } = proposal;
     const blockValid = Block.verifyBlock(block);
