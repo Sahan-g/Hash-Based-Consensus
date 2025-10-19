cadc2349161051e89fa1e862b8962b85a2537ea4
sahang <sahangawesh19@gmail.com>
2025-10-19 21:57:03 +0530

    increased the round time to two min

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const INITIAL_BALANCE = 500;
 const PROPOSER_REWARD = 50;
 const DIFFICULTY = 4;
 
-const ROUND_INTERVAL = 20 * 1000; // 20 seconds in ms
-const PHASE_1_DURATION = 4 * 1000; // First 4 seconds for publishing randomness
-const PHASE_3_START = 18 * 1000; // Leader publishes block at 18th minute
-const TRANSACTION_COLLECTION_DURATION = 16 * 1000; // Collect transactions for first 16 seconds
+const ROUND_INTERVAL = 120 * 1000; // 20 seconds in ms
+const PHASE_1_DURATION = 15 * 1000; // First 4 seconds for publishing randomness
+const PHASE_3_START = 100 * 1000; // Leader publishes block at 18th minute
+const TRANSACTION_COLLECTION_DURATION = 85 * 1000; // Collect transactions for first 16 seconds
 const CLEANUP_INDEX_FREQUENCY = 10; // every 10 indices clean the pending transactions
 const CLEANUP_LIMIT = 5; // cleanup transactions older than 5 rounds
 
